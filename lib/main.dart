589d4f1dd09f0fcb632d54fbebbef6df781eed8f
Gianthard-cyh <2953427626@qq.com>
2021-08-18 19:42:48 +0800

    fixed InkWell Added Color Management

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'widgets/search.dart';
 import 'package:badges/badges.dart';
 
+final BoxShadow normalShadow = BoxShadow(offset: Offset(0, 4));
+
 main() {
   runApp(OursApp());
 
@@ -64,31 +66,39 @@ class _OursAppBarState extends State<OursAppBar> {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Color(0xFFBE4749),
         child: Container(
             height: 56,
             decoration: BoxDecoration(color: Color(0xFFBE4749)),
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-              child: Container(
-                child: Row(
-                  children: [
-                    Expanded(child: OursSearchWidget()),
-                    SizedBox(width: 10),
-                    Badge(
-                        position: BadgePosition(top: 2, end: 8),
-                        badgeColor: Color(0xffE68E8F),
-                        child: InkResponse(
-                          child: IconButton(
-                              padding: EdgeInsets.all(0),
-                              iconSize: 32,
-                              color: Color(0xffE68E8F),
-                              icon: Icon(Icons.mail_outline),
-                              onPressed: () => {},
-                              tooltip: "123"),
-                        ))
-                  ],
-                ),
-              ),
-            )));
+            child: Material(
+                color: Color(0xFFBE4749),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Expanded(child: OursSearchWidget()),
+                        SizedBox(width: 10),
+                        Badge(
+                          position: BadgePosition(top: 2, end: 8),
+                          badgeColor: Color(0xffE68E8F),
+                          child: Center(
+                            child: Ink(
+                              decoration:
+                                  BoxDecoration(color: Color(0xFFBE4749)),
+                              child: IconButton(
+                                padding: EdgeInsets.all(0),
+                                icon: const Icon(Icons.mail_outline),
+                                iconSize: 32,
+                                color: Color(0xffE68E8F),
+                                onPressed: () {},
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ))));
   }
 }
---
+++ lib/widgets/card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OursCard extends StatefulWidget {
+  const OursCard({ Key? key }) : super(key: key);
+
+  @override
+  _OursCardState createState() => _OursCardState();
+}
+
+class _OursCardState extends State<OursCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
